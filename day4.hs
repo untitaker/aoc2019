253bcd83215d67bd8ed214abb00742e0ddf62355
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-12-04 20:02:21 +0100

    use more operators

--- day4.hs
+++ day4.hs
@@ -5,10 +5,10 @@ import Data.List
 (//) = div
 
 -- get nth digit from x counting from right (starting at 1)
-digitAt n x = x % (10 ^ n) // (10 ^ (n-1))
+x ! n = x % (10 ^ n) // (10 ^ (n-1))
 
 -- get all but leftmost digits of integer
-digitTail x n = x - ((digitAt n x) * 10 ^ (n - 1))
+digitTail x n = x - ((x ! n) * 10 ^ (n - 1))
 
 -- repeat digit x n times
 repeatDigit :: Int -> Int -> Int
@@ -21,14 +21,14 @@ generateDigits :: Int -> Int -> [Int]
 generateDigits 1 start = [start..9]
 generateDigits n start = sol
     where
-    minX = digitAt n start
+    minX = start ! n
     sol = [ x * (10 ^ (n-1)) + y | x <- [minX..9], y <- generateDigits (n-1) (getNewStart n start x) ]
 
 -- calculate new start position for recursive call of generateDigits
 getNewStart n start x | x == minX = max (digitTail start n) digitBounded
                       | otherwise = digitBounded
     where
-    minX = digitAt n start
+    minX = start ! n
     digitBounded = repeatDigit x (n-1)
 
 -- implement upper side of range... since everything is lazily computed we can
@@ -38,15 +38,15 @@ getRange digits start end = takeWhile (<= end) $ generateDigits digits start
 gotDubs 0 = False
 gotDubs x = found || next
     where
-    found = (digitAt 1 x) == (digitAt 2 x)
+    found = (x ! 1) == (x ! 2)
     next = gotDubs (x // 10)
 
 gotRealDubsImpl 0 prevDigit = False
 gotRealDubsImpl x prevDigit = found && hasBorders || next
     where
-    next = gotRealDubsImpl (x // 10) (digitAt 1 x)
-    found = (digitAt 1 x) == (digitAt 2 x) && hasBorders
-    hasBorders = prevDigit /= (digitAt 1 x) && (digitAt 2 x) /= (digitAt 3 x)
+    next = gotRealDubsImpl (x // 10) (x ! 1)
+    found = (x ! 1) == (x ! 2) && hasBorders
+    hasBorders = prevDigit /= (x ! 1) && (x ! 2) /= (x ! 3)
 
 gotRealDubs x = gotRealDubsImpl x (-1)
 
